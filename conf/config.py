d991cfdbf44f0e7227527faf86eb0b1228cdebea
cma2 <cma2@cisco.com>
2020-10-27 14:22:48 +0800

    first commit

---
+++ conf/config.py
@@ -0,0 +1,46 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Copyright 2020-present Works Mobile Corp.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import os
+
+# default language ['en'|'ko'|'ja']
+DEFAULT_LANG = 'ja'
+
+# TimeZone
+TZone = "Asia/Tokyo"
+
+# account
+ADMIN_ACCOUNT = "wo.08327@works-83443"
+DOMAIN_ID = 10175504
+
+# api
+API_ID = "jp1hipIEBeDUo"
+SERVER_CONSUMER_KEY = "uLKJLanbaiLODRd8M0s1"
+
+SERVER_ID = "24e72399c0214acb9eac5fc6d357f52b"
+SECRET_KEY_NAME = "private_20200326171020.key"
+
+# LOCAL ADDRESS
+LOCAL_ADDRESS = "https://app-bot-line.herokuapp.com/"
+
+# DB config
+DB_HOST = "ec2-3-211-48-92.compute-1.amazonaws.com"
+DB_NAME = "d8rcdqsb9ig9vs"
+DB_USER = "cooagdvsvfshlc"
+DB_PORT = "5432"
+DB_PASSWORD = "93e000dbfd1a6408da520845a0ca52f927e37456b747d0ea150af6b4724fce56"
